45deee2e3ece49bb7037d3354205bc977571c684
Connor Horman <chorman64@gmail.com>
2026-07-26 19:12:26 -0400

    Make the GOT type either be integer or pointer.

--- loader/src/loader.c
+++ loader/src/loader.c
@@ -6,8 +6,13 @@
 
 extern ElfNative_Dyn _DYNAMIC[];
 
-extern void* _GLOBAL_OFFSET_TABLE_[];
+union GotEntry {
+    void* address;
+    uintptr_t value;
+};
+
+extern union GotEntry _GLOBAL_OFFSET_TABLE_[];
 
 void* get_image_base_addr(void) {
-    return (void*) ((uintptr_t)(&_DYNAMIC) - (uintptr_t)_GLOBAL_OFFSET_TABLE_[0]);
+    return (void*) ((uintptr_t)(&_DYNAMIC) - _GLOBAL_OFFSET_TABLE_[0].value);
 }
